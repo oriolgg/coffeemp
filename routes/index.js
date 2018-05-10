4a311eafa3095ef91f8b8460e36ef0dd63cac230
Oriol Garcia <oriol.garcia@atresmediahf.com>
2018-05-10 14:36:38 +0200

    Adds /api prefix in the endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ module.exports = (app) => {
     /**
      * Index
      */
-    app.get('/v1/', (req, res) => res.json('ok'));
+    app.get('/api/v1/', (req, res) => res.json('ok'));
 
     /**
      * User
      */
-    app.use('/v1/users', require('./user/user'));
+    app.use('/api/v1/users', require('./user/user'));
 
     /**
      * Coffee
      */
-    app.use('/v1/coffees', require('./coffee/coffee'));
+    app.use('/api/v1/coffees', require('./coffee/coffee'));
 
     /**
      * Orders
      */
-    app.use('/v1/orders', require('./order/order'));
+    app.use('/api/v1/orders', require('./order/order'));
 
 };
