cdbcf27b4aab056e7e6d6f15a31202eb15b3103d
Oriol Garcia <oriol.garcia@atresmediahf.com>
2018-05-10 15:06:51 +0200

    Returns http 500 when there is an error creating a user

--- routes/user/controllers/create.js
+++ routes/user/controllers/create.js
@@ -2,6 +2,11 @@ const Repository = require('../../../repositories/user');
 
 module.exports = (req, res) => {
 
-    Repository.create(req.body.data.user, (data) => res.json(data));
+    Repository.create(req.body.data.user, (data) => {
+        if (data.error == true) {
+            return res.status(500).json(data);
+        }
+        return res.json(data);
+    });
 
 };
